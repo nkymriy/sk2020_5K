9e5eea0351f4d13a71fd6f00e639219dd12aa324
sakaiharuka <1801017@s.asojuku.ac.jp>
2020-06-24 11:02:04 +0900

    ADD signsコントローラの作成 　　 signup、signupファイルの作成

--- 04.source/ideash/config/routes.rb
+++ 04.source/ideash/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'signup/index'
+  get 'signin/index'
   root 'top#index'
   get 'home/index' => "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
