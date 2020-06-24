28befb32e19c02dc34a5113f7da19b7f813f7cdd
sakaiharuka <1801017@s.asojuku.ac.jp>
2020-06-24 10:55:39 +0900

    UPDATE top レイアウトの修正

--- 04.source/ideash/config/routes.rb
+++ 04.source/ideash/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'top/index'
-  root 'home#index'
+  root 'top#index'
+  get 'home/index' => "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
