93df2c031de82b8ca647b4f1a4095c63106cc5a4
ryunosuke <ryuryuryu713@gmail.com>
2020-06-24 13:14:48 +0900

    UPDATE マイグレーションを実行

--- 04.source/ideash/db/migrate/20200623035313_create_ideas.rb
+++ 04.source/ideash/db/migrate/20200623035313_create_ideas.rb
@@ -2,7 +2,7 @@ class CreateIdeas < ActiveRecord::Migration[6.0]
   def change
     create_table :ideas do |t|
 
-      t.t.belongs_to :idea_category, foreign_key: true
+      t.belongs_to :idea_category, foreign_key: true
       t.string :idea_name
       t.string :idea_description
       t.timestamps
--- 04.source/ideash/db/schema.rb
+++ 04.source/ideash/db/schema.rb
@@ -13,38 +13,54 @@
 ActiveRecord::Schema.define(version: 2020_06_23_040554) do
 
   create_table "idea_categories", force: :cascade do |t|
+    t.string "idea_category_name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "idea_logs", force: :cascade do |t|
+    t.integer "idea_id"
+    t.string "idea_log"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["idea_id"], name: "index_idea_logs_on_idea_id"
   end
 
   create_table "ideas", force: :cascade do |t|
+    t.integer "idea_category_id"
+    t.string "idea_name"
+    t.string "idea_description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "memos", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.index ["idea_category_id"], name: "index_ideas_on_idea_category_id"
   end
 
   create_table "user_ideas", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "idea_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["idea_id"], name: "index_user_ideas_on_idea_id"
+    t.index ["user_id"], name: "index_user_ideas_on_user_id"
   end
 
   create_table "user_logs", force: :cascade do |t|
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_user_logs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "user_name", null: false
+    t.string "user_mail", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "idea_logs", "ideas"
+  add_foreign_key "ideas", "idea_categories"
+  add_foreign_key "user_ideas", "ideas"
+  add_foreign_key "user_ideas", "users"
+  add_foreign_key "user_logs", "users"
 end
