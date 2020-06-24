08cc43c3865dbbebf012cd9814122f7a81867547
sakaiharuka <1801017@s.asojuku.ac.jp>
2020-06-24 11:26:18 +0900

    UPDATE 画面繊維だけ確認できるようにルーティングを追加

--- 04.source/ideash/config/routes.rb
+++ 04.source/ideash/config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  get 'signup/index'
-  get 'signin/index'
   root 'top#index'
+
   get 'home/index' => "home#index"
+
+  get 'signup/index' => "signs#signup"
+  get 'signin/index' => "signs#signin"
+
+  get '/signin/signup/index'=> "signs#signup"
+
+  #以下は臨時のルーティング
+  get '/signin/signup/home/index'=>"home#index"
+  get '/signin/home/index'=>"home#index"
+  get '/signup/home/index'=>"home#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
