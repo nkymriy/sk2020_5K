96474d82585f823f80cbcb99e36174b1fd609e03
ryunosuke <ryuryuryu713@gmail.com>
2020-06-24 11:16:02 +0900

    FIX create_user_ideas.rbの4行目が改行されていなかったのを修正

--- 04.source/ideash/db/migrate/20200623035543_create_user_ideas.rb
+++ 04.source/ideash/db/migrate/20200623035543_create_user_ideas.rb
@@ -1,6 +1,7 @@
 class CreateUserIdeas < ActiveRecord::Migration[6.0]
   def change
     create_table :user_ideas do |t|
+
       t.integer :user_id, foreign_key: true
       t.integer :idea_id, foreign_key: true
       t.timestamps
