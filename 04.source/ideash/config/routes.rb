98f5609e300c0b40f92888bc2aac628971597d78
Nakayama Reiya <nkymriy@gmail.com>
2020-06-29 02:36:25 +0900

    UPDATE routes

--- 04.source/ideash/config/routes.rb
+++ 04.source/ideash/config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  root 'top#index'
   get 'account/signin' => 'users#signin'
   get 'account/signup' => 'users#signup'
   get 'account/signout' =>'users#singout'
   get 'account/profile_edit' =>'users#edit'
-  get 'top/index'
-  root 'top#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
