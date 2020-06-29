8d4661a7acd822c37a8bb6e51c88d4cb0643f84b
Nakayama <1901029@s.asojuku.ac.jp>
2020-06-29 11:38:39 +0900

    FIX dbtestのnewを動くように修正

--- 04.source/ideash/config/routes.rb
+++ 04.source/ideash/config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'top/index'
   root 'top#index'
 
-  get 'dbtest/callertest'
   resources :dbtest
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
