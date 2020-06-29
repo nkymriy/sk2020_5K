fa5bf2fe86b66608cebea67be3f9912369f36e98
Nakayama <1901029@s.asojuku.ac.jp>
2020-06-29 12:12:31 +0900

    FIX dbtestのnewを動くように修正

--- 04.source/ideash/app/controllers/dbtest_controller.rb
+++ 04.source/ideash/app/controllers/dbtest_controller.rb
@@ -8,4 +8,9 @@ class DbtestController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(params.require(:user).permit(:user_name, :user_mail))
+    @user.save
+  end
+
 end
