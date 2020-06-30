174effb3821550ee10d1b988a044f1e79497c454
Nakayama <1901029@s.asojuku.ac.jp>
2020-06-30 09:57:20 +0900

    DELETE routesとViewsの重複分を削除

--- 04.source/ideash/config/routes.rb
+++ 04.source/ideash/config/routes.rb
@@ -23,16 +23,5 @@ Rails.application.routes.draw do
   resources :users
 
 
-  #TODO: 重複分を消す
-  get 'signup/index' => "signs#signup"
-  get 'signin/index' => "signs#signin"
-
-  get '/signin/signup/index'=> "signs#signup"
-
-  #以下は臨時のルーティング
-  get '/signin/signup/home/index'=>"home#index"
-  get '/signin/home/index'=>"home#index"
-  get '/signup/home/index'=>"home#index"
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
