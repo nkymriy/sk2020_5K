aa5c59855ba69eace3c439797c37b4359c964ee7
sakaiharuka <1801017@s.asojuku.ac.jp>
2020-06-30 11:21:54 +0900

    UPDATE ルーティングの整理 　　　  shared.cssを描写

--- 04.source/ideash/config/routes.rb
+++ 04.source/ideash/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   root 'top#index'
+  get 'top/index'=>"top#index"
 
   get 'home/index' => "home#index"
 
@@ -7,11 +9,6 @@ Rails.application.routes.draw do
   get 'signin/index' => "signs#signin"
 
   get '/signin/signup/index'=> "signs#signup"
-
-  #以下は臨時のルーティング
-  get '/signin/signup/home/index'=>"home#index"
-  get '/signin/home/index'=>"home#index"
-  get '/signup/home/index'=>"home#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
