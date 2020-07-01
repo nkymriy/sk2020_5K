e6f243b4e084baf1a8859876dc90ca7641cfc53b
ASO1701005 <1701005@st.asojuku.ac.jp>
2020-07-01 11:50:16 +0900

    add devise初期設定

--- 04.source/ideash/config/environments/development.rb
+++ 04.source/ideash/config/environments/development.rb
@@ -65,4 +65,16 @@ Rails.application.configure do
   config.web_console.whitelisted_ips = "0.0.0.0/0"
   config.hosts << 'team5.work'
 
+  # mail setting
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :user_name => "dkojilb@gmail.com",
+      :password => "0113",
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
+
 end
--- 04.source/ideash/config/routes.rb
+++ 04.source/ideash/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+      registrations: "users/registrations"
+  }
+  get 'welcome/index'
   get 'top/index'
   root 'top#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  ##match 'hello' => 'welcome#hello'
 end
