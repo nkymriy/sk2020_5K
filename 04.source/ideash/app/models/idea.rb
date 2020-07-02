eafd9a109cd8901f114a56c1ce904ca2b997b805
Nakayama <1901029@s.asojuku.ac.jp>
2020-07-02 14:32:35 +0900

    UPDATE annotateを導入
    modelファイルにDBの構造が記載されるようになった。
    migrateを実行したときに自動で追記される。

--- 04.source/ideash/app/models/idea.rb
+++ 04.source/ideash/app/models/idea.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: ideas
+#
+#  id               :integer          not null, primary key
+#  idea_description :string
+#  idea_name        :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  idea_category_id :integer
+#
+# Indexes
+#
+#  index_ideas_on_idea_category_id  (idea_category_id)
+#
+# Foreign Keys
+#
+#  idea_category_id  (idea_category_id => idea_categories.id)
+#
 class Idea < ApplicationRecord
   has_one :idea_logs
   has_many :user_idea
