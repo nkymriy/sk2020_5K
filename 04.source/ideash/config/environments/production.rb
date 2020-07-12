3ed3b573da54024cbc7bbd377d3c5ce57301327f
Nakayama <1901029@s.asojuku.ac.jp>
2020-07-13 04:57:41 +0900

    UPDATE mailerがEC2上で動くように修正

--- 04.source/ideash/config/environments/production.rb
+++ 04.source/ideash/config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
@@ -52,7 +52,7 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -82,9 +82,9 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
   # Do not dump schema after migrations.
@@ -116,8 +116,8 @@ Rails.application.configure do
   config.hosts << 'localhost'
   #devise用の設定
   #   #Action_mailerのデフォルトURLの設定
-  #   # config.action_mailer.default_url_options = { host: 'team5.work'}
-  config.action_mailer.default_url_options = {host: 'localhost', port: 3000}
+  config.action_mailer.default_url_options = {host: 'team5.work'}
+  # config.action_mailer.default_url_options = {host: 'localhost', port: 3000}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       #gmail利用時はaddress,domain,portは下記で固定
