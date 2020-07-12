6ce5c7a9586c9d5600d7924df554f26b55f21348
Nakayama <1901029@s.asojuku.ac.jp>
2020-07-13 03:16:38 +0900

    UPDATE annotateを更新

--- 04.source/ideash/config/routes.rb
+++ 04.source/ideash/config/routes.rb
@@ -16,7 +16,6 @@
 #                         idea_memo_new GET    /idea/memo/new(.:format)                                                                 memo#new
 #                                       POST   /idea/memo/new(.:format)                                                                 memo#new
 #                             idea_memo POST   /idea/memo(.:format)                                                                     memo#create
-#                        idea_memo_show GET    /idea/memo/show(.:format)                                                                memo#show
 #                                       GET    /idea/memo/edit/:id(.:format)                                                            memo#edit
 #                                       PATCH  /idea/memo(.:format)                                                                     memo#update
 #         rails_postmark_inbound_emails POST   /rails/action_mailbox/postmark/inbound_emails(.:format)                                  action_mailbox/ingresses/postmark/inbound_emails#create
