9d33304c375527de3d820a0015f1aad4dbac960b
ryunosuke <ryuryuryu713@gmail.com>
2020-07-17 14:47:33 +0900

    UPDATE 入力した値を受け取れるように

--- 04.source/ideash/app/channels/idea_channel.rb
+++ 04.source/ideash/app/channels/idea_channel.rb
@@ -1,12 +1,15 @@
 class IdeaChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    # 5.times { puts '***test***' }
+    stream_from "idea_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speak
+  def speak(data)
+    ActionCable.server.broadcast 'idea_channel', idea_log: data['idea_log']
   end
 end
--- 04.source/ideash/app/javascript/channels/idea_channel.js
+++ 04.source/ideash/app/javascript/channels/idea_channel.js
@@ -1,8 +1,9 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("IdeaChannel", {
+const chatChannel = consumer.subscriptions.create("IdeaChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
+    // console.log('test')
   },
 
   disconnected() {
@@ -11,9 +12,20 @@ consumer.subscriptions.create("IdeaChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
+    return alert(data['idea_log']);
   },
 
-  speak: function() {
-    return this.perform('speak');
+  speak: function(idea_log) {
+    return this.perform('speak', {
+      idea_log: idea_log
+        });
+  }
+});
+
+$(document).on('keypress', '[data-behavior~=idea_speaker]', function(event) {
+  if (event.keyCode === 13) {
+    chatChannel.speak(event.target.value);
+    event.target.value = '';
+    return event.preventDefault();
   }
 });
