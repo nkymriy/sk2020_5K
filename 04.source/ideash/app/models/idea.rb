d7c8a45558ccb3e2875b234435351687078382fa
ryunosuke <ryuryuryu713@gmail.com>
2020-07-23 10:30:09 +0900

    FIX idea.rbの「has_one :idea_logs」を「has_many :idea_logs」に修正

--- 04.source/ideash/app/models/idea.rb
+++ 04.source/ideash/app/models/idea.rb
@@ -10,7 +10,7 @@
 #  updated_at       :datetime         not null
 #
 class Idea < ApplicationRecord
-  has_one :idea_logs
+  has_many :idea_logs
   has_many :user_idea
   has_many :user, through: :user_idea
 end
