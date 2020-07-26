6762dcc742bfcbce258c01c7d0765092fc20f8e0
Nakayama <1901029@s.asojuku.ac.jp>
2020-07-27 04:53:28 +0900

    UPDATE createを動作するように変更

--- 04.source/ideash/app/controllers/brainstorming_controller.rb
+++ 04.source/ideash/app/controllers/brainstorming_controller.rb
@@ -3,6 +3,23 @@ class BrainstormingController < ApplicationController
   end
 
   def edit
+    @idea = Idea.find_by(id: params[:id])
+  end
+
+  def create
+    p params
+    user = User.find_by(id: current_user.id)
+    new_idea = user.idea.new do |idea|
+      idea.idea_category_id = 2
+      idea.idea_name = params[:theme]
+    end
+    if new_idea.save!
+      logger.debug "create new idea: #{new_idea.inspect}"
+      redirect_to idea_brainstorming_edit_url(id: new_idea.id)
+    else
+
+    end
+    # render action: :edit
   end
 
   def new
