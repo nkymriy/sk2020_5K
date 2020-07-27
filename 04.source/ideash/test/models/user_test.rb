78854f6a1190e87b555a3aba95e542b60d0e037b
ASO1701006 <1701006@st.asoujuku.ac.jp>
2020-07-27 11:39:37 +0900

    ADD rspec:install

--- 04.source/ideash/test/models/user_test.rb
+++ 04.source/ideash/test/models/user_test.rb
@@ -30,4 +30,12 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  # def setup
+  #   @user = User.new(user_name: "Example User", email: "user@example.com")
+  # end
+  #
+  # test "should be valid" do
+  #   assert @user.valid?
+  # end
 end
