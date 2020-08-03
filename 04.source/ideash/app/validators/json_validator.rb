0df21baa6cc59edd0f9b5fa94e7e83d03c38d08d
ryunosuke <ryuryuryu713@gmail.com>
2020-08-03 11:57:55 +0900

    UPDATE json_validator.rbのstrictオプションを削除

--- 04.source/ideash/app/validators/json_validator.rb
+++ 04.source/ideash/app/validators/json_validator.rb
@@ -1,6 +1,8 @@
 class JsonValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    JSON::Validator.validate!(options[:schema], value, strict: true)
+    # NOTE :strictをtrueにするとバリデーションが厳密になる
+    # JSON::Validator.validate!(options[:schema], value, strict: true)
+    JSON::Validator.validate!(options[:schema], value)
   rescue JSON::Schema::ValidationError => e
     record.errors[attribute] << (options[:message] || e.message)
   end
