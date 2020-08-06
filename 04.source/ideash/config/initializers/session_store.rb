8349c4c94eb919e20402dbf8882a68357aeb39a7
ryunosuke <ryuryuryu713@gmail.com>
2020-08-06 10:12:02 +0900

    FIX ローカル環境で動くようにsession_store.rbを修正

--- 04.source/ideash/config/initializers/session_store.rb
+++ 04.source/ideash/config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 Ideash::Application.config.session_store :redis_store, {
     servers: [
         {
-            host: "team5.work",  # Redisのサーバー名
+            host: "localhost",  # Redisのサーバー名
             port: 6379,         # Redisのサーバーのポート
             db: 0,              # データベースの番号(0 ~ 15)の任意
             namespace: "session"  # 名前空間。"session:セッションID"の形式
