ce4365413493539109ddf21396da5df40c3dc0cb
ASO1701006 <1701006@st.asoujuku.ac.jp>
2020-08-17 12:24:27 +0900

    ADD idea_logのテスト用ファイル生成

--- 04.source/ideash/spec/factories/idea_log.rb
+++ 04.source/ideash/spec/factories/idea_log.rb
@@ -1,1 +1,14 @@
+# Table name: idea_logs
+#
+#  id         :integer          not null, primary key
+#  idea_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  is_active  :boolean          default(FALSE), not null
+#  query      :json
 
+FactoryBot.define do
+  factory :idea_logs do
+    idea_id{1}
+  end
+end
