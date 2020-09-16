d2fff83553490c1279cc734a3da99fac7eeb9799
Naka <32997052+CatsManma@users.noreply.github.com>
2020-09-16 09:40:55 +0900

    不要なコメントやプリントを削除

--- 04.source/ideash/app/channels/idea_channel.rb
+++ 04.source/ideash/app/channels/idea_channel.rb
@@ -11,18 +11,11 @@ class IdeaChannel < ApplicationCable::Channel
 
   def speak(data)
     # ActionCable.server.broadcast 'idea_channel', idea_log: data['idea_log']
-    # IdeaLog.create! query: {'content': data['idea_log']}
     IdeaLog.create! idea_id: params[:idea], query: {'user_id': current_user.id,'mode': 'add', 'add': data['idea_log']}
   end
 
   def add(data)
-    # ActionCable.server.broadcast 'idea_channel', idea_log: data['idea_log']
-    # IdeaLog.create! query: {'content': data['idea_log']}
     res = ActiveRecord::Base.connection.execute("select count(*) from idea_logs where idea_id = '#{params[:idea]}' and JSON_EXTRACT(query, '$.mode') = 'add' ")
-    p "-----------#{res}-----------"
-    p "-----------#{res[0]}-----------"
-    p "-----------#{res[0]['count(*)']}-----------"
-    p "-----------#{DateTime.now}-----------"
     # TODO Timeを保存する処理を追加する
     IdeaLog.create! idea_id: params[:idea], query: {'user_id': current_user.id,'mode': 'add', 'add': { 'object_id': res[0]['count(*)'], 'content': data["content"]}, 'time': DateTime.now}
   end
