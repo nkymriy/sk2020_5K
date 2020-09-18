bf8d56e0e03b811af6dcbd1dbec766211c3088a4
ASO1701005 <1701005@st.asojuku.ac.jp>
2020-09-18 15:11:23 +0900

    UPDATE jobsコードクリーン

--- 04.source/ideash/app/jobs/idealog_broadcast_job.rb
+++ 04.source/ideash/app/jobs/idealog_broadcast_job.rb
@@ -3,14 +3,11 @@ class IdealogBroadcastJob < ApplicationJob
 
   def perform(idea_log)
     # Do something later
-    # ActionCable.server.broadcast 'idea_channel', idea_log: render_idealog(idea_log)
-    # ActionCable.server.broadcast "idea_channel_#{idea_log.idea_id}", idealogs: render_idealog(idea_log)
     ActionCable.server.broadcast "idea_channel_#{idea_log.idea_id}", idea_logs: render_idealog(idea_log)
 
   end
 
   def render_idealog(idea_log)
-    # ApplicationController.renderer.render partial: 'ideachat/idealogs', locals: { idealogs: idea_log.query }
     idea_log.query
   end
 end
