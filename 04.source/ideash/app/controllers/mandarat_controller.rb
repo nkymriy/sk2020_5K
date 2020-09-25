421d4bb1e52e12f368e1d99a519eb618da304ef6
ASO1701005 <1701005@st.asojuku.ac.jp>
2020-09-25 14:33:28 +0900

    UPDATE create,edit追加

--- 04.source/ideash/app/controllers/mandarat_controller.rb
+++ 04.source/ideash/app/controllers/mandarat_controller.rb
@@ -1,4 +1,23 @@
 class MandaratController < ApplicationController
   def index
   end
+  def edit
+    @idea = Idea.find_by(id: params[:id])
+    @idea_logs = @idea.idea_logs
+    @idea_users = @idea.user
+  end
+  def create
+    p params
+    user = User.find_by(id: current_user.id)
+    new_idea = user.idea.new do |idea|
+      idea.idea_category_id = 3
+      idea.idea_name = params[:theme]
+    end
+    if new_idea.save!
+      logger.debug "create new idea: #{new_idea.inspect}"
+      redirect_to idea_mandarat_edit_url(id: new_idea.id)
+    else
+
+    end
+  end
 end
