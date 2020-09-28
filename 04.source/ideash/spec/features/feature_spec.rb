7692d63494e54f126d85097e7a0f714967bc0000
ASO1701006 <1701006@st.asoujuku.ac.jp>
2020-09-28 15:04:54 +0900

    ADD メモまで

--- 04.source/ideash/spec/features/feature_spec.rb
+++ 04.source/ideash/spec/features/feature_spec.rb
@@ -1,24 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe "Feature", type: :feature do
-  before do
+  before only_path:true do
     #user作成
-    #User.create!(email:'develop1@example.com',password:'password')
-
-    #ユーザを作成
-    # @user = User.create(
-    #     email:"develop1@example.com",
-    #     passsword:"password"
-    # )
+    User.create(email:'develop1@example.com',password:'password')
   end
   scenario "ブラウザ操作をシュミレートする" do
-
     #トップページにアクセス
     visit root_path
 
     #signinページへ遷移
-    within("div.btns_1_1") do
+    within('div.btns_1_1') do
       click_on 'Sign In'
     end
+
+    #ここから/account/signin
+    #遷移に成功したかの検証(/account/signin)
+    expect(page).to have_content 'Sign-In to your account'
+    expect(current_path).to eq '/account/signin'
+    #loginフォームにemailとpasswordを入力する
+    fill_in 'email-id',with: 'develop1@example.com'
+    fill_in 'password-id',with: 'password'
+    click_on 'Sign In'
+
+    #ここから/idea/home
+    #loginして遷移に成功したかの検証(/idea/home)
+    expect(current_path).to eq '/idea/home'
+    click_link '新規作成'
+
+    #ここから/idea/category
+    #遷移に成功したか検証(/idea/category)
+    expect(current_path).to eq '/idea/category'
+    click_link 'メモ'
+    expect(current_path).to eq '/idea/memo/new'
+    #headerのIdeashを押しidea/homeに戻る
+    click_link 'Ideash'
+    expect(current_path).to eq '/idea'
+
+    #ここから/idea/home
+    click_link 'ブレインストーミング(β)'
+
+    #ここからidea/brainstorming/new
+    expect(current_path).to eq '/idea/brainstorming/new'
+    # fill_in 'theme',with: 'feature-test'
+    # click_on 'はじめる'
+    # expect(current_path).to eq '/idea/brainstorming/edit/'
+    click_link 'Ideash'
+    expect(current_path).to eq '/idea'
+
+    #ここからidea/home
+    click_link 'メモ'
+
+    #ここからidea/memo/new
+    expect(current_path).to eq '/idea/memo/new'
+    fill_in 'メモに名前をつけてください',with: 'feature-test'
+    fill_in 'メモを書きましょう',with: 'feature-memo'
+    click_on '保存'
+    within all('div.memos').last do
+      ##click_link 'feature-test'
+      #expect(page).to have_selector 'div.memo-text',text: 'feature-test'
+      #find('div.memo-text',text: 'feature-test')
+      #あとでやる
+    end
+    expect(page).to have_content 'メモを保存しました。'
+    click_link 'Ideash'
+    expect(current_path).to eq '/idea'
+
+    #ここからidea/home
+    click_link '履歴'
+
+    #ここからidea/history
+    expect(current_path).to eq '/idea/history'
+
   end
 end
+
