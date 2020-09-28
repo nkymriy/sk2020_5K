719e55f171ea19e288778d360b2f476c013259ca
Nakayama <nkymriy@gmail.com>
2020-09-28 14:39:32 +0900

    UPDATE CSVを読み込む処理を追加

--- 04.source/ideash/app/controllers/top_controller.rb
+++ 04.source/ideash/app/controllers/top_controller.rb
@@ -3,6 +3,7 @@ class TopController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    @csv = CSV.read("app/assets/csv/release_note.csv", headers: true, return_headers: false)
   end
 
   def release_note
