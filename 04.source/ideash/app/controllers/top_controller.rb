cffa70f62f824198fc23174bb5b8d9d675a58fc8
Nakayama <nkymriy@gmail.com>
2020-09-28 12:04:40 +0900

    UPDATE Controller側でrelease_note.csvを読み込むように変更

--- 04.source/ideash/app/controllers/top_controller.rb
+++ 04.source/ideash/app/controllers/top_controller.rb
@@ -1,6 +1,11 @@
 class TopController < ApplicationController
+  require 'csv'
   skip_before_action :authenticate_user!
 
   def index
+  end
+
+  def release_note
+    @csv = CSV.read("app/assets/csv/release_note.csv", headers: true, return_headers: false)
   end
 end
