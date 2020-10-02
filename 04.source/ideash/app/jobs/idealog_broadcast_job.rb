81b6cebdaf8a729495b9a8c870ac6eff2e724ab4
ryunosuke <ryuryuryu713@gmail.com>
2020-10-02 14:03:41 +0900

    UPDATE idealog_broadcast_job.rb内の命名がえらいことになってたのを修正

--- 04.source/ideash/app/jobs/idealog_broadcast_job.rb
+++ 04.source/ideash/app/jobs/idealog_broadcast_job.rb
@@ -1,13 +1,13 @@
 class IdealogBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform(idea_log)
+  def perform(json_idea_log)
     # Do something later
-    ActionCable.server.broadcast "idea_channel_#{idea_log.idea_id}", idea_logs: render_idealog(idea_log)
+    ActionCable.server.broadcast "idea_channel_#{json_idea_log.idea_id}", idea_logs: get_idea_log_query(json_idea_log)
 
   end
 
-  def render_idealog(idea_log)
-    idea_log.query
+  def get_idea_log_query(json_idea_log)
+    json_idea_log.query
   end
 end
