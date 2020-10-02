cb67a739026bb31d9b99293bb78d3f69b416b136
ryunosuke <ryuryuryu713@gmail.com>
2020-10-02 13:54:59 +0900

    UPDATE idea_logのqueryにjoinが複数保存される問題を修正

--- 04.source/ideash/app/channels/idea_channel.rb
+++ 04.source/ideash/app/channels/idea_channel.rb
@@ -19,6 +19,14 @@ class IdeaChannel < ApplicationCable::Channel
   end
 
   def join_user()
+    res = ActiveRecord::Base.connection.execute("select count(*) from idea_logs where idea_id = '#{params[:idea]}' and JSON_EXTRACT(query, '$.mode') = 'join' and JSON_EXTRACT(query, '$.user_id') = #{current_user.id}")
+    if res[0]['count(*)'] > 0
+      return
+    end
     IdeaLog.create! idea_id: params[:idea], query: {'user_id': current_user.id, 'mode': 'join', 'join': {'user_mail': current_user.email}}
+  end
+
+  def set_timer()
+    IdeaLog.create! idea_id: params[:idea], query: {'user_id': current_user.id, 'mode': 'system', 'system': {'operation': 'sample_operation', 'option': 'sample_option'}}
   end
 end
