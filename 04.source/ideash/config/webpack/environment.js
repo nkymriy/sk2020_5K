0c0b63adf2dc615123ae23380c3b837e151a2f49
Nakayama <nkymriy@gmail.com>
2020-10-08 10:56:54 +0900

    UPDATE #96

--- 04.source/ideash/config/webpack/environment.js
+++ 04.source/ideash/config/webpack/environment.js
@@ -3,16 +3,9 @@ const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery'
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
     })
-);
-
-const aliasConfig = {
-    'jquery': 'jquery/src/jquery',
-    'jquery-ui': 'jquery-ui-dist/jquery-ui.js'
-};
-
-environment.config.set('resolve.alias', aliasConfig);
+)
 
 module.exports = environment
