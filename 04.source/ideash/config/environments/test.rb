20903e6c26ff6c7a8c2c66eca3e691f9ef42910d
ASO1701005 <1701005@st.asojuku.ac.jp>
2020-10-09 14:42:21 +0900

    UPDATE キャッシュ情報のログを取得

--- 04.source/ideash/config/environments/test.rb
+++ 04.source/ideash/config/environments/test.rb
@@ -23,6 +23,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local = true
   config.action_controller.perform_caching = true
+  config.action_controller.enable_fragment_cache_logging = true
   config.cache_store = :null_store
 
   # Raise exceptions instead of rendering exception templates.
