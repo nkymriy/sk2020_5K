220ac1046bb1ce957e9a163fa49ed620366c7938
ASO1701005 <1701005@st.asojuku.ac.jp>
2020-10-13 12:26:39 +0900

    UPDATE 事前コンパイル

--- 04.source/ideash/config/environments/production.rb
+++ 04.source/ideash/config/environments/production.rb
@@ -134,4 +134,19 @@ Rails.application.configure do
       authentication: :login,
       :enable_starttls_auto => true
   }
+
+  #Assetsの事前コンパイル
+  # コンパイル済みアセットを圧縮するかどうかを指定。圧縮することで、データ容量を減らし、高速化をする
+  config.assets.compress = true
+
+  # JavaScriptの圧縮に使用するプログラムを定義。:uglifierが最も圧縮率が高い
+  # config.assets.js_compressor = :uglifier
+
+  # CSSの圧縮に使用するプログラムを定義
+  config.assets.css_compressor = :scss
+
+  # 動的なSprocketsコンパイルをするかどうかを指定
+  # config.assets.compile = false
+
+  config.assets.debug = false
 end
