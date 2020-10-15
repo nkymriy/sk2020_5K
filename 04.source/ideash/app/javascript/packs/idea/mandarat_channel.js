63d69f060c264ec3662030ac3585dfe1693837f1
ASO1701005 <1701005@st.asojuku.ac.jp>
2020-10-15 10:48:18 +0900

    UPDATE とりあえず

--- 04.source/ideash/app/javascript/packs/idea/mandarat_channel.js
+++ 04.source/ideash/app/javascript/packs/idea/mandarat_channel.js
@@ -19,11 +19,13 @@ $(document).on("turbolinks:load", function () {
             received(idea_log) {
 
                 let query = idea_log['idea_logs']
+                let val = parseInt(localStorage.getItem('radio_value'));
 
                 //編集時
                 if (query['mode'] === 'editing') {
                     //IdeaLog.where(is_editing: '1')
-                    let first = '.t'; let sel_num = query["editing"]["object_id"];
+                    let first = '.t';
+                    let sel_num = + parseInt(query["editing"]["object_id"]) - val;
                     let sel_class = first + sel_num;
                     //console.log (sel_class);
                     $(sel_class).css('background-color', '#C0C0C0');
@@ -31,7 +33,8 @@ $(document).on("turbolinks:load", function () {
                 }
                 //確定時
                 else if (query['mode'] === 'edit') {
-                    let first1 = '.t'; let first2 = 'zoom_'; let sel_num = query["edit"]["object_id"];
+                    let first1 = '.t'; let first2 = 'zoom_';
+                    let sel_num = parseInt(query["edit"]["object_id"])-val;
                     let sel_class = first1 + sel_num;
                     let sel_id = first2 + sel_num;
                     $(sel_class).css('background-color', '#FFFFFF');
@@ -57,15 +60,15 @@ $(document).on("turbolinks:load", function () {
                 $('#zoom_'+i)
                     //テキストボックスにフォーカス時
                     .focusin(function(event) {
-                        var val = localStorage.getItem('radio_value');
-                        console.log(val);
-                        let content = { object_id: i };
+                        let val = parseInt(localStorage.getItem('radio_value'));
+                        let content = { object_id: val+i};
                         console.log(content);
                         consumer.task.editing(content);
                     })
                     //テキストボックスからフォーカス外したとき
                     .focusout(function(event) {
-                        let content = { object_id: i, content: event.delegateTarget.value };
+                        let val = parseInt(localStorage.getItem('radio_value'));
+                        let content = { object_id: val+i, content: event.delegateTarget.value };
                         console.log(content);
                         consumer.task.edit(content);
                     });
