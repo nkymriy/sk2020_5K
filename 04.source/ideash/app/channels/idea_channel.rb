138eb2f686d354145932e7268e024ab3098a4cad
ASO1701005 <1701005@st.asojuku.ac.jp>
2020-10-16 14:53:06 +0900

    UPDATE joinの追加

--- 04.source/ideash/app/channels/idea_channel.rb
+++ 04.source/ideash/app/channels/idea_channel.rb
@@ -19,6 +19,10 @@ class IdeaChannel < ApplicationCable::Channel
   end
 
   def join_user()
+    res = ActiveRecord::Base.connection.execute("select count(*) from idea_logs where idea_id = '#{params[:idea]}' and JSON_EXTRACT(query, '$.mode') = 'join' and JSON_EXTRACT(query, '$.user_id') = #{current_user.id}")
+    if res[0]['count(*)'] > 0
+      return
+    end
     IdeaLog.create! idea_id: params[:idea], query: {'user_id': current_user.id, 'mode': 'join', 'join': {'user_mail': current_user.email}}
   end
 
