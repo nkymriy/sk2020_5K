65f7bff41b73da5b942f77f7373992c2578ef6c7
ASO1701005 <1701005@st.asojuku.ac.jp>
2020-10-20 14:55:19 +0900

    UPDATE 2次元配列に保持

--- 04.source/ideash/app/javascript/packs/idea/mandarat_channel.js
+++ 04.source/ideash/app/javascript/packs/idea/mandarat_channel.js
@@ -35,32 +35,41 @@ $(document).on("turbolinks:load", function () {
                     let sel_num = + parseInt(query["editing"]["object_id"]) - val;
                     let sel_class = first + sel_num;
                     //console.log (sel_class);
+
                     $(sel_class).css('background-color', '#C0C0C0');
-                    console.log("focusin");
+                    //console.log("focusin");
                 }
                 //確定時
                 else if (query['mode'] === 'edit') {
                     let text = query["edit"]["content"];
-                    let first1 = '.t'; let first2 = 'zoom_';
+                    let first1='.t'; let first2='zoom_';
                     let bigid = query["edit"]["object_id"];
                     let minid = bigid - val;
                     let sel_class = first1 + minid;
                     let sel_id = first2 + minid;
-                    //$(sel_class).css('background-color', '#FFFFFF');
-                    //console.log(sel_class);
-                    document.getElementById(sel_id).value = text;
+                    $(sel_class).css('background-color', '#FFFFFF');
                     $('#'+bigid).text(text);
-                    console.log(bigid);
-                    //console.log(sel_id);
-                    console.log("focusout");
 
-                    //メインテーマ,サブテーマを表示させるとこ
                     let left = String(bigid).slice(0,1);
                     let right = String(bigid).slice(-1);
+
+                    if(localStorage.getItem('flg') !== '0') {
+                        //document.getElementById(sel_id).value = text;
+                        if(left=='4'||right=='4' && bigid!=='44' && bigid!=='4') {
+                            //alert('a');
+                            localStorage.setItem('flg','0');
+                        }
+                    } else if(localStorage.getItem('flg') == '0') {
+                        let sel_id2 = first2 + left;
+                        //document.getElementById(sel_id2).value = text;
+                        localStorage.setItem('flg','1');
+                    }
+                    //メインテーマ,サブテーマを表示させるとこ
                     if(bigid == '44') { $('#main').text(text); }
                     else if(bigid == '4') { $('#theme0').text(text); }
                     else if (left ==  '4') { $('#theme'+right).text(text); }
                     else if(bigid % 10 == 4){ $('#theme'+left).text(text); }
+                    //console.log("focusout");
                 }
             },
 
@@ -74,29 +83,69 @@ $(document).on("turbolinks:load", function () {
             }
         });
 
+        let array = [];
+
+
         //ズームイン画面の処理
         $(function() {
             for(let i=0; i<=8; i++) {
+                var flg = false;
                 $('#zoom_'+i)
                     //テキストボックスにフォーカス時
                     .focusin(function(event) {
                         let val = parseInt(localStorage.getItem('radio_value'));
-                        let content = { object_id: val+i};
+                        let objid = val+i;
+                        let content = { object_id: objid };
                         console.log(content);
                         consumer.task.editing(content);
+
+                        let left = String(objid).slice(0,1);
+                        let right = String(objid).slice(-1);
+                        if((left=='4'||right=='4')&& objid !== 44) {
+                            let turnnum = right + left;
+                            if(objid==40) { turnnum = 4 }
+                            else if(objid==4) { turnnum = 40 }
+                            turnnum = parseInt(turnnum);
+                            content = { object_id: turnnum };
+                            console.log(content);
+                            consumer.task.editing(content);
+                            flg = !flg;
+                            console.log(turnnum);
+                        }
+                        //console.log('in'+flg);
                     })
                     //テキストボックスからフォーカス外したとき
                     .focusout(function(event) {
                         let val = parseInt(localStorage.getItem('radio_value'));
-                        let content = { object_id: val+i, content: event.delegateTarget.value };
+                        let text = event.delegateTarget.value;
+                        let objid = val+i;
+                        let content = { object_id: objid, content: text };
+                        //contentデータ保持
+                        array.push([objid,text]);
                         console.log(content);
                         consumer.task.edit(content);
+                        if(flg == true) {
+                            let left = String(val+i).slice(0,1);
+                            let right = String(val+i).slice(-1);
+                            let objid = right + left;
+                            objid = parseInt(objid);
+                            let content = { object_id: objid, content: text };
+                            //contentデータ保持
+                            array.push([objid,text]);
+                            console.log(content);
+                            consumer.task.edit(content);
+                            flg = !flg;
+                        }
+                        //console.log('out'+flg);
+                        //contentデータ保持
+                        console.log(array);
                     });
             }
         });
 
         //左上のミニマップ選択された後
         $('input:radio[name="hoge"]').change(function() {
+            console.log()
             const value = $('input:radio[name="hoge"]:checked').val();
             console.log(value);
             localStorage.setItem('radio_value', value);
@@ -114,20 +163,17 @@ $(document).on("turbolinks:load", function () {
                 if(radioval >= 0) {
                     $('input[value='+radioval+']').prop('checked', true).change();
                 }
-            }
-            function leftbtn() {
+            }function leftbtn() {
                 let radioval = parseInt(localStorage.getItem('radio_value'))-10;
                 if(radioval % 30 !== 20) {
                     $('input[value='+radioval+']').prop('checked', true).change();
                 }
-            }
-            function rightbtn() {
+            }function rightbtn() {
                 let radioval = parseInt(localStorage.getItem('radio_value'))+10;
                 if(radioval % 30 !== 0) {
                     $('input[value=' + radioval + ']').prop('checked', true).change();
                 }
-            }
-            function downbtn() {
+            }function downbtn() {
                 let radioval = parseInt(localStorage.getItem('radio_value'))+30;
                 if(radioval <= 80) {
                     $('input[value=' + radioval + ']').prop('checked', true).change();
