b4e2c62627c7c813fdc48066a98f3a9e7490e9ab
Nakayama <nkymriy@gmail.com>
2020-10-20 10:09:44 +0900

    UPDATE JSをjsファイルに移植

---
+++ 04.source/ideash/app/javascript/packs/shared/_editmenu.js
@@ -0,0 +1,33 @@
+$(document).on("turbolinks:load", function () {
+    require('jquery-ui/ui/widgets/draggable')
+    $('.chat').draggable();
+
+    function set2fig(num) {
+        // 桁数が1桁だったら先頭に0を加えて2桁に調整する
+        var ret;
+        if (num < 10) {
+            ret = "0" + num;
+        } else {
+            ret = num;
+        }
+        return ret;
+    }
+
+    const showClock2 = function(){
+        var nowTime = new Date();
+        var nowHour = set2fig(nowTime.getHours());
+        var nowMin = set2fig(nowTime.getMinutes());
+        var nowSec = set2fig(nowTime.getSeconds());
+        var msg = nowHour + ":" + nowMin + ":" + nowSec;
+        if ($('#RealtimeClockArea2').length === 0) {
+            clearInterval(timerId)
+        } else {
+            document.getElementById("RealtimeClockArea2").innerHTML = msg;
+        }
+    }
+    console.log("test:1")
+    if ($(".brainstorming.edit").length > 0) {
+        console.log("test:2")
+        let timerId = setInterval(showClock2, 1000);
+    }
+});
