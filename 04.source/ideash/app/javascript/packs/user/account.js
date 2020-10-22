1d1b4124fa7e3462781411792dda267de6a6b08e
ASO1701006 <1701006@st.asoujuku.ac.jp>
2020-10-22 10:36:51 +0900

    ADD toastで更新された名前を表示

--- 04.source/ideash/app/javascript/packs/user/account.js
+++ 04.source/ideash/app/javascript/packs/user/account.js
@@ -2,12 +2,13 @@ require('fomantic-ui-css/semantic.min');
 $(function(){
     let is_updated = $('#is_updated').val() ?? false;
     console.log (is_updated);
+    let user_name = $('#account_input').val()
 
     if(is_updated == 'true'){
         $('body')
             .toast({
                 title: 'SUCCESS',
-                message: '更新に成功しました',
+                message: user_name + 'に更新しました',
                 showProgress: 'bottom',
                 classProgress: 'blue'
             });
