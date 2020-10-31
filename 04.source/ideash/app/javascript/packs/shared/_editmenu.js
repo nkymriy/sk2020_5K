c26d7502a78be781b999eea4f139f1bdb676f7a5
NakayamaReiya <nkymriy@gmail.com>
2020-10-31 19:51:36 +0900

    UPDATE _editmenuに招待ボタンを実装(クリックでURLをコピー)

--- 04.source/ideash/app/javascript/packs/shared/_editmenu.js
+++ 04.source/ideash/app/javascript/packs/shared/_editmenu.js
@@ -1,30 +1,61 @@
 $(function () {
     require('jquery-ui/ui/widgets/draggable')
     $('.chat').draggable();
-
-    function set2fig(num) {
-        // 桁数が1桁だったら先頭に0を加えて2桁に調整する
-        var ret;
-        if (num < 10) {
-            ret = "0" + num;
-        } else {
-            ret = num;
-        }
-        return ret;
-    }
-
-    const showClock2 = function () {
-        var nowTime = new Date();
-        var nowHour = set2fig(nowTime.getHours());
-        var nowMin = set2fig(nowTime.getMinutes());
-        var nowSec = set2fig(nowTime.getSeconds());
-        var msg = nowHour + ":" + nowMin + ":" + nowSec;
-        if ($('#RealtimeClockArea2').length === 0) {
-            clearInterval(timerId)
-        } else {
-            document.getElementById("RealtimeClockArea2").innerHTML = msg;
-        }
-    }
+    // let clipboard = new Clipboard('.copy');
     let timerId = setInterval(showClock2, 1000);
+    showClock2(timerId)
 
+    $('#copy_invite_link').on('click', function (e) {
+        // 空div 生成
+        var tmp = document.createElement("div");
+        // 選択用のタグ生成
+        var pre = document.createElement('pre');
+
+        // 親要素のCSSで user-select: none だとコピーできないので書き換える
+        pre.style.userSelect = 'auto';
+
+        tmp.appendChild(pre).textContent = $(e.target).val();
+
+        // 要素を画面外へ
+        var s = tmp.style;
+        s.position = 'fixed';
+        s.right = '200%';
+
+        // body に追加
+        document.body.appendChild(tmp);
+        // 要素を選択
+        document.getSelection().selectAllChildren(tmp);
+
+        // クリップボードにコピー
+        var result = document.execCommand("copy");
+
+        // 要素削除
+        document.body.removeChild(tmp);
+
+        return result;
+    });
 });
+
+function set2fig(num) {
+    // 桁数が1桁だったら先頭に0を加えて2桁に調整する
+    var ret;
+    if (num < 10) {
+        ret = "0" + num;
+    } else {
+        ret = num;
+    }
+    return ret;
+}
+
+function showClock2(timerId) {
+    var nowTime = new Date();
+    var nowHour = set2fig(nowTime.getHours());
+    var nowMin = set2fig(nowTime.getMinutes());
+    var nowSec = set2fig(nowTime.getSeconds());
+    var msg = nowHour + ":" + nowMin + ":" + nowSec;
+    if ($('#RealtimeClockArea2').length === 0) {
+        clearInterval(timerId)
+    } else {
+        document.getElementById("RealtimeClockArea2").innerHTML = msg;
+    }
+}
