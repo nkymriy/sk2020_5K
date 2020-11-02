a1ff8c2235d32d8553be8e718834b636683cf594
ryunosuke <ryuryuryu713@gmail.com>
2020-11-02 12:10:04 +0900

    UPDATE #180 無制限モードにチェックを入れた際に、時間の設定を変更できないように更新

--- 04.source/ideash/app/javascript/packs/brainstorming/new.js
+++ 04.source/ideash/app/javascript/packs/brainstorming/new.js
@@ -2,14 +2,26 @@
 // NOTE: テーマを入力しないとアイデアを作成できないようにする処理
 $("input[id='theme']").blur(function () {
     if (!$(this).val().match(/\S/g)) {
-        $(this).css("background-color", "#FEF4F8");
+        $(this).css('background-color', '#FEF4F8');
         $('#start_button').prop('disabled', true)
     } else {
-        $(this).css("background-color", "#FaFEFF");
+        $(this).css('background-color', '#FaFEFF');
         $('#start_button').prop('disabled', false)
     }
 });
 
-$('#theme').keydown(function(event) {
+$('#theme').keydown(function (event) {
     $('#start_button').prop('disabled', false)
+})
+
+$('#is_unlimited').click(function() {
+    if (this.checked) {
+        $('#process1').prop('disabled', true)
+        $('#process2').prop('disabled', true)
+        $('#process3').prop('disabled', true)
+    } else {
+        $('#process1').prop('disabled', false)
+        $('#process2').prop('disabled', false)
+        $('#process3').prop('disabled', false)
+    }
 })
