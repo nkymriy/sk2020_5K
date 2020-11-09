36fa3b2eeef0f075167c3c0e79a85928fd4164b9
Nakayama <nkymriy@gmail.com>
2020-11-09 22:22:21 +0900

    UPDATE レスポンシブとサイドバー周りの大規模な修正

--- 04.source/ideash/app/javascript/packs/shared.js
+++ 04.source/ideash/app/javascript/packs/shared.js
@@ -1,34 +1,47 @@
+require('fomantic-ui-css/semantic.min');
+
 $(function () {
     //読み込み時の表示
-    $('#home_body').fadeIn(500);
-    window_load();
-//ウィンドウサイズ変更時に更新
-    window.onresize = window_load;
+    $('#main_content').fadeIn(500);
+    console.log($("#main_content"))
+    $('#left_menu_show_button').on('click', function () {
+        $('#left_menu').show();
+        $('#left_icon_menu').hide()
+    });
+    $('#left_menu_hide_button').on('click', function () {
+        $('#left_menu').hide();
+        $('#left_icon_menu').show()
+    });
 
-//サイズの表示
-    function window_load() {
-        var display_Width = window.parent.screen.width;
-        var window_Width = window.innerWidth;
-        var key_Width = display_Width / 3 * 2;
-
-        let home_menu = document.getElementById('home_menu');
-        let edit_menu = document.getElementById('edit_menu');
-        let home_body = document.getElementById('home_body');
-        let edit_body = document.getElementById('edit_body');
-        let idea_card = document.getElementById('ideas');
-
-        if (window_Width <= key_Width) {
-            home_menu.className = 'ui left fixed small vertical inverted menu';
-            if (home_body) home_body.className = 'home_body thin_margin_left';
-            if (edit_body) edit_body.className = 'home_body thin_margin_right thin_margin_left';
-            if (edit_menu) edit_menu.className = 'ui right fixed vertical small menu block-edit-sidebar';
-            if (idea_card) idea_card.classList.replace('four', 'two');
-        } else {
-            home_menu.className = 'ui left fixed vertical inverted menu';
-            if (home_body) home_body.className = 'home_body wide_margin_left';
-            if (edit_body) edit_body.className = 'home_body wide_margin_right wide_margin_left';
-            if (edit_menu) edit_menu.className = 'ui right fixed vertical menu block-edit-sidebar';
-            if (idea_card) idea_card.classList.replace('two', 'four');
-        }
-    }
+    console.log($('.popup').popup());
+//     window_load();
+// //ウィンドウサイズ変更時に更新
+//     window.onresize = window_load;
+//
+// //サイズの表示
+//     function window_load() {
+//         var display_Width = window.parent.screen.width;
+//         var window_Width = window.innerWidth;
+//         var key_Width = display_Width / 3 * 2;
+//
+//         let left_menu = document.getElementById('left_menu');
+//         let left_menu = document.getElementById('left_menu');
+//         let home_body = document.getElementById('home_body');
+//         let edit_body = document.getElementById('edit_body');
+//         let idea_card = document.getElementById('ideas');
+//
+//         if (window_Width <= key_Width) {
+//             left_menu.className = 'ui left fixed small vertical inverted menu';
+//             if (home_body) home_body.className = 'home_body thin_margin_left';
+//             if (edit_body) edit_body.className = 'home_body thin_margin_right thin_margin_left';
+//             if (left_menu) left_menu.className = 'ui right fixed vertical small menu block-edit-sidebar';
+//             if (idea_card) idea_card.classList.replace('four', 'two');
+//         } else {
+//             left_menu.className = 'ui left fixed vertical inverted menu';
+//             if (home_body) home_body.className = 'home_body wide_margin_left';
+//             if (edit_body) edit_body.className = 'home_body wide_margin_right wide_margin_left';
+//             if (left_menu) left_menu.className = 'ui right fixed vertical menu block-edit-sidebar';
+//             if (idea_card) idea_card.classList.replace('two', 'four');
+//         }
+//     }
 });
