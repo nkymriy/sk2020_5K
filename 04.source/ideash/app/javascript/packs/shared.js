c4a75e094e409913ff46ed6684b11daf77bddd4e
NakayamaReiya <nkymriy@gmail.com>
2020-11-11 09:49:43 +0900

    UPDATE コメントを追加

--- 04.source/ideash/app/javascript/packs/shared.js
+++ 04.source/ideash/app/javascript/packs/shared.js
@@ -4,6 +4,8 @@ $(function () {
     //読み込み時の表示
     $('#main_content').fadeIn(500);
     $('.popup').popup();
+
+    //サイドバーの開閉ボタン
     $('#left_menu_show_button').on('click', function () {
         $('#left_menu').show();
         $('#left_icon_menu').hide()
@@ -21,35 +23,4 @@ $(function () {
         $('#right_menu').hide();
         $('#right_icon_menu').show()
     });
-
-//     window_load();
-// //ウィンドウサイズ変更時に更新
-//     window.onresize = window_load;
-//
-// //サイズの表示
-//     function window_load() {
-//         var display_Width = window.parent.screen.width;
-//         var window_Width = window.innerWidth;
-//         var key_Width = display_Width / 3 * 2;
-//
-//         let left_menu = document.getElementById('left_menu');
-//         let left_menu = document.getElementById('left_menu');
-//         let home_body = document.getElementById('home_body');
-//         let edit_body = document.getElementById('edit_body');
-//         let idea_card = document.getElementById('ideas');
-//
-//         if (window_Width <= key_Width) {
-//             left_menu.className = 'ui left fixed small vertical inverted menu';
-//             if (home_body) home_body.className = 'home_body thin_margin_left';
-//             if (edit_body) edit_body.className = 'home_body thin_margin_right thin_margin_left';
-//             if (left_menu) left_menu.className = 'ui right fixed vertical small menu block-edit-sidebar';
-//             if (idea_card) idea_card.classList.replace('four', 'two');
-//         } else {
-//             left_menu.className = 'ui left fixed vertical inverted menu';
-//             if (home_body) home_body.className = 'home_body wide_margin_left';
-//             if (edit_body) edit_body.className = 'home_body wide_margin_right wide_margin_left';
-//             if (left_menu) left_menu.className = 'ui right fixed vertical menu block-edit-sidebar';
-//             if (idea_card) idea_card.classList.replace('two', 'four');
-//         }
-//     }
 });
