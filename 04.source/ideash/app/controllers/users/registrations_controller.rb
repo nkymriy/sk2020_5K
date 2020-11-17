d42f3ac9c1e736e4a65dcb57b45c43ad2fe7d0bb
ryunosuke <ryuryuryu713@gmail.com>
2020-11-17 12:20:31 +0900

    FIX #254 Sign-Up画面でleft_menuが見えている不具合を修正

--- 04.source/ideash/app/controllers/users/registrations_controller.rb
+++ 04.source/ideash/app/controllers/users/registrations_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  layout 'head_left_layout'
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
