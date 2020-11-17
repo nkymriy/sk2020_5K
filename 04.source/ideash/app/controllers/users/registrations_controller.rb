5dcb1201de15a81b5ad06d00eba3dab24eb51b3b
ryunosuke <ryuryuryu713@gmail.com>
2020-11-17 12:47:45 +0900

    FIX #254 アカウント設定画面でleft_menuが消えていた不具合を修正

--- 04.source/ideash/app/controllers/users/registrations_controller.rb
+++ 04.source/ideash/app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  layout 'head_left_layout', only: :profile_edit
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
