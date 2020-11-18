9a00a092ad6e532a583a6f69c32c6421b0b09cfa
sakaiharuka <1801017@s.asojuku.ac.jp>
2020-11-18 10:52:36 +0900

    UPDATE　js完成

--- 04.source/ideash/app/javascript/packs/idea/word_slot.js
+++ 04.source/ideash/app/javascript/packs/idea/word_slot.js
@@ -4,7 +4,6 @@ $(document).on("turbolinks:load", function () {
     //ガチャを回す
     $('#turn-slot-button').on('click', turnbtn);
     let gacha_num = 3;
-    let display_word_num = 0;
     let word_list
 
     function turnbtn() {
@@ -15,61 +14,14 @@ $(document).on("turbolinks:load", function () {
             success: function(data){
                 console.log(gacha_num)
                 word_list = data;
-                display_word_num = 0;
-                display_rank();
-                document.getElementById("slot-word").innerHTML=word_list[display_word_num][1];
+                document.getElementById("slot-word-left").innerHTML=word_list[0][1];
+                document.getElementById("slot-word-center").innerHTML=word_list[1][1];
+                document.getElementById("slot-word-right").innerHTML=word_list[2][1];
+
             },
             error: function(data){
                 //失敗時の処理
             }
         });
-    }
-
-
-    //移動ボタン
-    $('#to-previous-button').on('click', previousbtn);
-    $('#to-next-button').on('click', nextbtn);
-
-    function previousbtn() {
-        if (display_word_num == 0){
-            display_word_num = gacha_num;
-        }
-        display_word_num = display_word_num - 1;
-        display_rank();
-        document.getElementById("slot-word").innerHTML=word_list[display_word_num][1];
-    }
-    function nextbtn() {
-        if (display_word_num == gacha_num - 1){
-            display_word_num = - 1;
-        }
-        display_word_num = display_word_num + 1;
-        display_rank();
-        document.getElementById("slot-word").innerHTML=word_list[display_word_num][1];
-    }
-    function display_rank() {
-        let rank_top = document.getElementById("rank_top");
-        let rank_bottom = document.getElementById("rank_bottom");
-
-        if (word_list[display_word_num][2]<=10000){
-            rank_top.className = 'N_rank';
-            rank_bottom.className = 'N_rank';
-            rank_top.innerHTML="N";
-            rank_bottom.innerHTML="N";
-        } else if (word_list[display_word_num][2]<=15000){
-            rank_top.className = 'R_rank';
-            rank_bottom.className = 'R_rank';
-            rank_top.innerHTML="R";
-            rank_bottom.innerHTML="R";
-        } else if (word_list[display_word_num][2]<=16000){
-            rank_top.className = 'SR_rank';
-            rank_bottom.className = 'SR_rank';
-            rank_top.innerHTML="SR";
-            rank_bottom.innerHTML="SR";
-        } else {
-            rank_top.className = 'SSR_rank';
-            rank_bottom.className = 'SSR_rank';
-            rank_top.innerHTML="SSR";
-            rank_bottom.innerHTML="SSR";
-        }
     }
 });
