04448c3d329b399a5f885d8d9aee5cfbd34efe94
Nakayama <nkymriy@gmail.com>
2020-11-19 10:53:15 +0900

    UPDATE デバッグ用の文を削除

--- 04.source/ideash/app/javascript/packs/common/check_controller_action.js
+++ 04.source/ideash/app/javascript/packs/common/check_controller_action.js
@@ -2,8 +2,5 @@ export const checkControllerAction = function ( permit_controllers, permit_actio
     let $body = $('body')
     const controller = $body.data('controller')
     const action = $body.data('action')
-    console.log(`${controller}#${action}`)
-    const rt = permit_controllers.indexOf(controller) >= 0 && permit_actions.indexOf(action) >= 0
-    if(rt)console.log(`active ${permit_controllers} # ${permit_actions}`)
     return (permit_controllers.indexOf(controller) >= 0 && permit_actions.indexOf(action) >= 0);
 }
