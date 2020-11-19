8f3e3b2caa69f38d0bd88f253222895faf8c680b
Nakayama <nkymriy@gmail.com>
2020-11-19 09:51:33 +0900

    UPDATE brainstorming.jsでjsの読み込みを修正

--- 04.source/ideash/app/javascript/packs/application.js
+++ 04.source/ideash/app/javascript/packs/application.js
@@ -14,6 +14,13 @@ require("jquery-ui")
 // fomantic-uiの読み込み
 import 'fomantic-ui-css/semantic.min.css'
 
+// 共有js
+require('./common/check_controller_action')
+
+// 各コントローラー用
+// idea新規作成画面
+require('./common/new')
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
--- 04.source/ideash/app/javascript/packs/common/new.js
+++ 04.source/ideash/app/javascript/packs/common/new.js
@@ -1,28 +1,33 @@
-// NOTE: テーマを入力しないとアイデアを作成できないようにする処理
+import {checkControllerAction} from "./check_controller_action";
+
 $(document).on("turbolinks:load", function () {
-    $("input[id='theme']").blur(function () {
-        if (!$(this).val().match(/\S/g)) {
-            $(this).css('background-color', '#FEF4F8');
-            $('#start_button').prop('disabled', true)
-        } else {
-            $(this).css('background-color', '#FaFEFF');
+    if (checkControllerAction(['brainstorming','mandarat'],['new'])) {
+        console.log("active")
+        // NOTE: テーマを入力しないとアイデアを作成できないようにする処理
+        $("input[id='theme']").blur(function () {
+            if (!$(this).val().match(/\S/g)) {
+                $(this).css('background-color', '#FEF4F8');
+                $('#start_button').prop('disabled', true)
+            } else {
+                $(this).css('background-color', '#FaFEFF');
+                $('#start_button').prop('disabled', false)
+            }
+        });
+
+        $('#theme').keydown(function (event) {
             $('#start_button').prop('disabled', false)
-        }
-    });
+        })
 
-    $('#theme').keydown(function (event) {
-        $('#start_button').prop('disabled', false)
-    })
-
-    $('#is_unlimited').click(function () {
-        if (this.checked) {
-            $('#process1').prop('disabled', true)
-            $('#process2').prop('disabled', true)
-            $('#process3').prop('disabled', true)
-        } else {
-            $('#process1').prop('disabled', false)
-            $('#process2').prop('disabled', false)
-            $('#process3').prop('disabled', false)
-        }
-    });
+        $('#is_unlimited').click(function () {
+            if (this.checked) {
+                $('#process1').prop('disabled', true)
+                $('#process2').prop('disabled', true)
+                $('#process3').prop('disabled', true)
+            } else {
+                $('#process1').prop('disabled', false)
+                $('#process2').prop('disabled', false)
+                $('#process3').prop('disabled', false)
+            }
+        });
+    }
 });
