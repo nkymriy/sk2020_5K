74da6406615b5c4bf259533054c1d6c43a2f80df
sakaiharuka <1801017@s.asojuku.ac.jp>
2020-11-27 15:04:34 +0900

    UPDATE ガチャ回せるようにした

--- 04.source/ideash/app/javascript/packs/idea/word_gacha.js
+++ 04.source/ideash/app/javascript/packs/idea/word_gacha.js
@@ -3,24 +3,19 @@ import {checkControllerAction} from "../common/check_controller_action";
 $(document).on("turbolinks:load", function () {
     checkControllerAction(['word_gacha'],['edit'])
     $('.ui.dropdown').dropdown()
+
     //ガチャを回す
     $('#turn-gacha-button').on('click', turnbtn);
-    let gacha_num = 0;
-    let display_word_num = 0;
     let word_list
 
     function turnbtn() {
-        gacha_num= $('#gacha-select').val();
-
         $.ajax({
             url: "get_wordgacha_jsons",
             type: "GET",
-            data: {gacha_num:gacha_num},
+            data: {gacha_num:10},
             success: function(data){
                 word_list = data;
-                display_word_num = 0;
-                display_rank();
-                document.getElementById("gacha-word").innerHTML=word_list[display_word_num][1];
+                display_cards();
             },
             error: function(data){
                 //失敗時の処理
@@ -28,51 +23,38 @@ $(document).on("turbolinks:load", function () {
         });
     }
 
+    function display_cards() {
 
-    //移動ボタン
-    $('#to-previous-button').on('click', previousbtn);
-    $('#to-next-button').on('click', nextbtn);
+        let rank_class;
+        let rank_text;
 
-    function previousbtn() {
-        if (display_word_num == 0){
-            display_word_num = gacha_num;
-        }
-        display_word_num = display_word_num - 1;
-        display_rank();
-        document.getElementById("gacha-word").innerHTML=word_list[display_word_num][1];
-    }
-    function nextbtn() {
-        if (display_word_num == gacha_num - 1){
-            display_word_num = - 1;
-        }
-        display_word_num = display_word_num + 1;
-        display_rank();
-        document.getElementById("gacha-word").innerHTML=word_list[display_word_num][1];
-    }
-    function display_rank() {
-        let rank_top = document.getElementById("rank_top");
-        let rank_bottom = document.getElementById("rank_bottom");
+        for(let i=0;i<10;i++){
+            console.log(word_list)
 
-        if (word_list[display_word_num][2]<=10000){
-            rank_top.className = 'N_rank';
-            rank_bottom.className = 'N_rank';
-            rank_top.innerHTML="N";
-            rank_bottom.innerHTML="N";
-        } else if (word_list[display_word_num][2]<=15000){
-            rank_top.className = 'R_rank';
-            rank_bottom.className = 'R_rank';
-            rank_top.innerHTML="R";
-            rank_bottom.innerHTML="R";
-        } else if (word_list[display_word_num][2]<=16000){
-            rank_top.className = 'SR_rank';
-            rank_bottom.className = 'SR_rank';
-            rank_top.innerHTML="SR";
-            rank_bottom.innerHTML="SR";
-        } else {
-            rank_top.className = 'SSR_rank';
-            rank_bottom.className = 'SSR_rank';
-            rank_top.innerHTML="SSR";
-            rank_bottom.innerHTML="SSR";
+            if (word_list[i][2]<=10000){
+                rank_class = 'N_rank';
+                rank_text="N";
+            } else if (word_list[i][2]<=15000){
+                rank_class = 'R_rank';
+                rank_text="R";
+            } else if (word_list[i][2]<=16000){
+                rank_class = 'SR_rank';
+                rank_text="SR";
+            } else {
+                rank_class = 'SSR_rank';
+                rank_text="SSR";
+            }
+
+            let newElement =
+                '<div class="word-card">' +
+                '<div class="rank_top '+rank_class+'">'+rank_text+'</div>' +
+                '<h1>'+word_list[i][1]+'</h1>' +
+                '<div class=rank_bottom "'+rank_class+'">'+rank_text+'</div>' +
+                '</div>'
+            ;
+
+            $(newElement).appendTo('#word-cards');
+
         }
     }
 });
