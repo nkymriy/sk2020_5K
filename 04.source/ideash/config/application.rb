2ed262064fe35b2b325132ef44aa1121eded7630
ryunosuke <ryuryuryu713@gmail.com>
2020-12-18 12:29:33 +0900

    FIX レイアウトが崩れる問題を修正

--- 04.source/ideash/config/application.rb
+++ 04.source/ideash/config/application.rb
@@ -22,5 +22,7 @@ module Ideash
     config.time_zone = 'Tokyo'
     # deviseで利用する言語を日本語に
     config.i18n.default_locale = :ja
+    # 自動挿入を防ぐように
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
 end
